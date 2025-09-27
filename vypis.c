aa09b0b9fd37387bbf28c6c30b51c82bfd010929
Matej Herich <herich.matej2005@gmail.com>
2025-09-27 11:23:49 +0200

    Vypis ASCI tabulky

---
+++ vypis.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+unsigned int pocitadlo;
+for(pocitadlo = 0;pocitadlo<=256;pocitadlo++){
+    printf("Na hodnote %d je v ASCI tabulke zapisany znak %c\n",pocitadlo,pocitadlo);
+
+}
+return 0;
+}
